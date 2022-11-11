8071fc6670d2dfd6ac33a0e4daea9d66da748bf0
PaoloGouba <pfgouba@gmail.com>
2022-11-11 20:07:36 +0100

    update transformer

--- src/transformer.py
+++ src/transformer.py
@@ -5,7 +5,6 @@ The transformer will run tranformation
 
 """
 
-import xsl
 import lxml.etree as t
 
 
@@ -16,13 +15,17 @@ class Transformer():
     
     def run_transformation(xml_input,xsl_input):
         
-        xml_feed = open(xml_input, encoding="utf8")
-        xsl_stylesheet = open(xsl_input)
+        xml_feed = open(str(xml_input), encoding="utf8")
+        print(xml_feed)
+        xsl_stylesheet = open(str(xsl_input))
+        print(xsl_input)
         feed = t.parse(xml_feed)
         xslt = t.parse(xsl_stylesheet)
+        transform = ''
 
         try :
             transform = t.XSLT(xslt)
+            print(transform)
         except : Exception 
            
         final_xml = transform(feed)
@@ -30,3 +33,10 @@ class Transformer():
         return final_xml
 
         
+        
+amazon_transformer = Transformer 
+
+export = r'C:\Users\PaoloGouba\OneDrive - BeezUP\Documents\software_development\amazon_marketplace_feed_generator\src\export.xml'
+xslt = r'C:\Users\PaoloGouba\OneDrive - BeezUP\Documents\software_development\amazon_marketplace_feed_generator\src\xsl\main.xsl'
+export.replace('\\','')
+amazon_transformer.run_transformation(export,xslt)      
